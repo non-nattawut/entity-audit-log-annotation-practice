674543f1971817f09eaf893205c48d3e0f8eca84
non-nattawut <nattavutnon@gmail.com>
2026-04-07 13:13:05 +0700

    first commit

---
+++ src/main/java/com/udong/entity_audit_log_annotation/EntityAuditLogAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.udong.entity_audit_log_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityAuditLogAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityAuditLogAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udong/entity_audit_log_annotation/EntityAuditLogAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udong.entity_audit_log_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityAuditLogAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
